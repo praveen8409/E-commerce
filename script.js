1481ade0237964b198b45cffb9228ece5c8b2532
Praveen Kumar <praveen.kumar.paytm2019@gmail.com>
2023-07-18 23:23:53 +0530

    I make responsive home page

---
+++ script.js
@@ -0,0 +1,16 @@
+const bar = document.querySelector("#bar");
+const nav = document.querySelector("#navbar");
+const close = document.querySelector("#close");
+console.log(close);
+
+if(bar){
+    bar.addEventListener('click', ()=>{
+        nav.classList.add('active');
+    });
+}
+
+if(close){
+    close.addEventListener('click', ()=>{
+        nav.classList.remove('active');
+    });
+}
